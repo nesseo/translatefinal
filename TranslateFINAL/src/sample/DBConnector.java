b88b1f227b4d848cf07c16d2432580a96981dc8b
nesseo <48216174+nesseo@users.noreply.github.com>
2019-03-28 08:05:41 -0400

    Add files via upload

---
+++ TranslateFINAL/src/sample/DBConnector.java
@@ -0,0 +1,17 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+    public static Connection getConnection (){
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:translate.db");
+            return connection;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
